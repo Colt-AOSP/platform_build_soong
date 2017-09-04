cd892ee81cdacbae3cbdd1e75aa2ad4be3d217e8
Simon Shields <simon@lineageos.org>
2017-09-04 21:09:44 +1000

    soong: explicitly specify name of COLT variables struct
    Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- android/variable.go
+++ android/variable.go
@@ -97,8 +97,8 @@ type variableProperties struct {
 			Enabled *bool
 		}
 
-		// include CypherOS variables
-		*android.Product_variables
+		// include COLT variables
+		Colt android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -173,8 +173,8 @@ type productVariables struct {
 
 	DeviceKernelHeaders []string `json:",omitempty"`
 
-	// include CypherOS variables
-	*android.ProductVariables
+	// include COLT variables
+	Colt android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
